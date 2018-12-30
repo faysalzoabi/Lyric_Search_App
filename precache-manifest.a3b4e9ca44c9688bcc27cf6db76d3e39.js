e55a9740bab306cafc5369364f82bcabf34c1788
Faysal AlZoubi <faysal.zoabi@gmail.com>
2018-12-30 11:00:23 +0100

    Updates

---
+++ precache-manifest.a3b4e9ca44c9688bcc27cf6db76d3e39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18b6e9487a819e94925a",
+    "url": "/Lyric_Search_App/static/js/runtime~main.18b6e948.js"
+  },
+  {
+    "revision": "f1d7492d29e908c67778",
+    "url": "/Lyric_Search_App/static/js/main.f1d7492d.chunk.js"
+  },
+  {
+    "revision": "56240ba2085a98bbd529",
+    "url": "/Lyric_Search_App/static/js/1.56240ba2.chunk.js"
+  },
+  {
+    "revision": "f1d7492d29e908c67778",
+    "url": "/Lyric_Search_App/static/css/main.f7a343d2.chunk.css"
+  },
+  {
+    "revision": "9dadaa120351d3ac95a7c36bc8f4123c",
+    "url": "/Lyric_Search_App/index.html"
+  }
+];
